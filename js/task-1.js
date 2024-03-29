49ef6aa677dd5a2618b49fa62abc40426b68c3d2
Oleg Troian <sleite4r@gmail.com>
2024-03-29 14:44:31 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+//* Task 1 Задача 1. Генератор slug
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+//* Усі символи slug повинні бути в нижньому регістрі.
+//* Усі слова slug повинні бути розділені тире.
+
+function slugify(title) {
+  const words = title.split(" ");
+  return words.join("-").toLowerCase()
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
